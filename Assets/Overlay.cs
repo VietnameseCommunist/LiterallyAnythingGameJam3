8369d7bd811ad7fed8a8fa9864d2d362d51c6fa6
JALBOD <benetone8@gmail.com>
2025-08-05 08:43:44 +0300

    UI stuff

--- Assets/Overlay.cs
+++ Assets/Overlay.cs
@@ -8,7 +8,7 @@ public class GradientOverlay : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-         if (!Instance) Instance = this;
+         if (Instance == null) Instance = this;
     }
 
     // Update is called once per frame
--- Assets/ReloadingUI.cs
+++ Assets/ReloadingUI.cs
@@ -5,13 +5,14 @@ public class ReloadingUI : MonoBehaviour
 {
      public static ReloadingUI Instance;
 
+     public static bool interruptus;
+
      public Image img;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
          if (!Instance) Instance = this;
     }
-
 
     public static void StartReload(float time)
     {
@@ -21,18 +22,28 @@ public class ReloadingUI : MonoBehaviour
               {
                    Instance.gameObject.SetActive(true);
               }
+              interruptus = true;
               Instance.StartCoroutine(Instance.Reload(time));
          }
     }
 
     public IEnumerator Reload(float time)
     {
+         yield return null;
+         if (interruptus)
+         {
+              interruptus = false;
+         }
          float elapsedTime = 0;
          while (time > elapsedTime)
          {
               yield return null;
               elapsedTime += Time.deltaTime;
               Debug.Log(elapsedTime);
+              if (interruptus)
+              {
+                   yield break;
+              }
               if (img) img.fillAmount = Mathf.Lerp(0, 1, elapsedTime / time);
          }
          img.fillAmount = 1;
@@ -41,14 +52,29 @@ public class ReloadingUI : MonoBehaviour
          {
               transform.localScale = Vector3.one * animation;
               animation += Time.deltaTime * transform.localScale.magnitude;
+              if (interruptus)
+              {
+                   transform.localScale = Vector3.one;
+                   yield break;
+              }
               yield return null;
          }
          while (animation > 0)
          {
               transform.localScale = Vector3.one * animation;
               animation -= Time.deltaTime / (transform.localScale.magnitude / 2);
+              if (interruptus)
+              {
+                   transform.localScale = Vector3.one;
+                   yield break;
+              }
               yield return null;
          }
+         DisableThis();
+    }
+
+    void DisableThis()
+    {
          transform.localScale = Vector3.one;
          gameObject.SetActive(false);
     }
